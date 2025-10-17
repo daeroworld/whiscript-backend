ce70961818112e4dd1d6225cc98249d323a60e75
daeroworld <daeroworld@gmail.com>
2025-10-17 22:43:28 +0900

    ✨FEAT: Initialize monolithic service
    - Add main entry point in `cmd/main.go` to start the HTTP server using Gin
    - Implement dependency injection container in `configuration/di.go` to wire router, routes, and service dependencies
    - Define `Variable` struct in `secret.go` to store basic service configuration like database URI and API ports
    - Add local `replace` directive in `go.mod` to point `github.com/daeroworld/shared` to local path
    - Include shared and Gin dependencies in `go.mod` and `go.sum`

---
+++ monolithic/configuration/di.go
@@ -0,0 +1,70 @@
+package configuration
+
+import (
+	"fmt"
+	"net/http"
+	
+	voiceWriter "voice/writer/configuration"
+
+	sharedConfiguration "github.com/daeroworld/shared/configuration"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type Container struct {
+	Router      *gin.Engine
+	Variable    *Variable
+	voiceWriter sharedConfiguration.IContainer
+}
+
+func (c *Container) GetHttpHandler() http.Handler {
+	return c.Router.Handler()
+}
+
+func (c *Container) InitVariable() error {
+	c.Variable = NewVariable()
+	return nil
+}
+
+func (c *Container) SetRouter(router any) {
+	c.Router = router.(*gin.Engine)
+
+	front := fmt.Sprintf("http://%s:%d", c.Variable.Frontend.Ip, c.Variable.Frontend.Port)
+	c.Router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{front}, // Allow frontend domain
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+}
+
+func (c *Container) DefineDatabase(databaseWrappers ...any) error {
+
+	return nil
+}
+
+func (c *Container) DefineRoute(router interface{}) error {
+	c.voiceWriter.DefineRoute(router)
+	return nil
+}
+
+func (c *Container) DefineGrpc() error {
+	panic("not implemented")
+}
+
+func (c *Container) InitDependency(db interface{}) error {
+	c.voiceWriter = voiceWriter.NewMonoContainer()
+	
+	return nil
+}
+
+func NewContainer() *Container {
+	ctnr := &Container{}
+	ctnr.InitVariable()
+	ctnr.DefineDatabase(nil)
+	ctnr.InitDependency(nil)
+	router := gin.Default()
+	ctnr.SetRouter(router)
+	ctnr.DefineRoute(router)
+	return ctnr
+}
